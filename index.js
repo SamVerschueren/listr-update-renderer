c7e7fa23e697961388032e338dc0fcb8c095c360
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2019-09-14 11:56:14 +0000

    fixed linter issues

--- index.js
+++ index.js
@@ -44,18 +44,18 @@ const renderHelper = (tasks, options, level) => {
 	return output.join('\n');
 };
 
-const shouldSuspendUpdateRenderer = (tasks) => {
-
-	for(const task of tasks){
-		if(task.isEnabled() && task.isPending() && !task.isCompleted()){
+const shouldSuspendUpdateRenderer = tasks => {
+	for (const task of tasks) {
+		if (task.isEnabled() && task.isPending() && !task.isCompleted()) {
 			return task.shouldSuspendUpdateRenderer();
 		}
 	}
-}
+};
 
 const render = (tasks, options) => {
-	if(!shouldSuspendUpdateRenderer(tasks))
+	if (!shouldSuspendUpdateRenderer(tasks)) {
 		logUpdate(renderHelper(tasks, options));
+	}
 };
 
 class UpdateRenderer {
